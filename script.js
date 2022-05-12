436ff847e1b4048c05f92eccbe3a5daeb24be846
Max McDonough <max.mcdonough@gmail.com>
2022-05-12 14:43:54 -0400

    Added arrays for possible characters (v1.2)

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Define parameters of possible characters based on user input
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const special = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+
 // Write password to the #password input
 function writePassword() {
 
-// Prompt user for desired length of password
+  // Prompt user for desired length of password
   var passwordLength = prompt("Hello. Please select a character length for your password. Password must be between 8 and 128 characters in length.");
 
   // Check to make sure user entered a number
@@ -16,23 +22,18 @@ function writePassword() {
   while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("Please try again. Password must be between 8 and 128.");
   }
-// Ask user if they want to use lower case characters
-  var lowerCase = confirm("Would you like to use lower case characters in your password? Click OK for Yes, Cancel for No.");
+  // Ask user if they want to use lower case characters
+  var wantLowerCase = confirm("Would you like to use lower case characters in your password? Click OK for Yes, Cancel for No.");
 
   // Ask user if they want to use lower case characters
-  var upperCase = confirm("Would you like to use upper case characters in your password? Click OK for Yes, Cancel for No.");
+  var wantUpperCase = confirm("Would you like to use upper case characters in your password? Click OK for Yes, Cancel for No.");
 
-// Ask user if they want to use lower case characters
+  // Ask user if they want to use lower case characters
   var wantNumbers = confirm("Would you like to use numbers in your password? Click OK for Yes, Cancel for No.");
 
-// Ask user if they want to use lower case characters
+  // Ask user if they want to use lower case characters
   var wantSpecial = confirm("Would you like to use special characters in your password? Click OK for Yes, Cancel for No.");
   
-  
-
-
-
-
   var password = generatePassword();
   
   var passwordText = document.querySelector("#password");
